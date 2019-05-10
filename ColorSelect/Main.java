14592f3b736b473736ab91e2ec1b531751ccb20a
Vinícius Clemente <48721419+jjotaplayer@users.noreply.github.com>
2019-05-10 11:05:40 -0300

    Update Main.java
    Simples Update tirando algumas coisas

--- ColorSelect/Main.java
+++ ColorSelect/Main.java
@@ -10,12 +10,6 @@ public class Main {
 	public static void main(String[] args) {
 		ColorSelectFrame frame = new ColorSelectFrame(); 
 	      frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	      frame.getContentPane().setBackground( Color.GRAY );
-	      frame.setSize( 500, 300 ); // set frame size
 	      frame.setVisible( true );
-	      
-
-
 	}
-
 }
