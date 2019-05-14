00fb784be218b2d7d326d10abd9d5d1a9410e0ae
Vinícius Clemente <48721419+jjotaplayer@users.noreply.github.com>
2019-05-14 11:44:35 -0300

    Create Main.java

---
+++ Printer/Main.java
@@ -0,0 +1,15 @@
+package Printer;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FramePrincipal frame = new FramePrincipal();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(550, 200);
+		frame.setVisible(true);
+
+	}
+
+}
